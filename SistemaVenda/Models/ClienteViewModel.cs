582951a5eb61b5f62c882c2645f7622f3531afbd
Roberto Prado <robertopfigue@gmail.com>
2020-07-15 05:09:24 -0300

    Adicionado o CRUD para Cliente.

---
+++ SistemaVenda/Models/ClienteViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVenda.Models
+{
+    public class ClienteViewModel
+    {
+        public int? Codigo { get; set; }
+
+        [Required(ErrorMessage="Informe o Nome do Cliente!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe o CNPJ/CPF do Cliente!")]
+        public string CNPJ_CPF { get; set; }
+
+        [Required(ErrorMessage = "Informe o Email do Cliente!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Informe o Celular do Cliente!")]
+        public string Celular { get; set; }
+    }
+}
