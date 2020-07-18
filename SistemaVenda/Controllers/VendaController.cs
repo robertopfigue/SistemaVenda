b0c1073321aaef94bb690647d177f18949156583
Roberto Prado <robertopfigue@gmail.com>
2020-07-18 05:30:31 -0300

    Adicionado o CRUD para as vendas.

---
+++ SistemaVenda/Controllers/VendaController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using SistemaVenda.DAL;
+using SistemaVenda.Entidades;
+using SistemaVenda.Models;
+
+namespace SistemaVenda.Controllers
+{
+    public class VendaController : Controller
+    {
+        protected ApplicationDbContext mContext;
+
+        public VendaController(ApplicationDbContext context)
+        {
+            mContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Venda> lista = mContext.Venda.ToList();
+            mContext.Dispose();
+            return View(lista);
+        }
+
+        private IEnumerable<SelectListItem> ListaProduto()
+        {
+            List<SelectListItem> lista = new List<SelectListItem>();
+
+            lista.Add(new SelectListItem()
+            {
+                Value = string.Empty,
+                Text = string.Empty
+            });
+
+            foreach (var item in mContext.Produto.ToList())
+            {
+                lista.Add(new SelectListItem()
+                {
+                    Value = item.Codigo.ToString(),
+                    Text = item.Descricao.ToString()
+                });
+            }
+
+            return lista;
+        }
+
+        private IEnumerable<SelectListItem> ListaCliente()
+        {
+            List<SelectListItem> lista = new List<SelectListItem>();
+
+            lista.Add(new SelectListItem()
+            {
+                Value = string.Empty,
+                Text = string.Empty
+            });
+
+            foreach (var item in mContext.Cliente.ToList())
+            {
+                lista.Add(new SelectListItem()
+                {
+                    Value = item.Codigo.ToString(),
+                    Text = item.Nome.ToString()
+                });
+            }
+
+            return lista;
+        }
+
+        [HttpGet]
+        public IActionResult Cadastro(int? id)
+        {
+            VendaViewModel viewModel = new VendaViewModel();
+            viewModel.ListaClientes = ListaCliente();
+            viewModel.ListaProdutos = ListaProduto();
+
+            if (id != null)
+            {
+                var entidade = mContext.Venda.Where(x => x.Codigo == id).FirstOrDefault();
+                viewModel.Codigo = entidade.Codigo;
+                viewModel.Data = entidade.Data;
+                viewModel.CodigoCliente = entidade.CodigoCliente;
+                viewModel.Total = entidade.Total;
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(VendaViewModel entidade)
+        {
+            if (ModelState.IsValid)
+            {
+                Venda objVenda = new Venda()
+                {
+                    Codigo = entidade.Codigo,
+                    Data = (DateTime)entidade.Data,
+                    CodigoCliente = (int)entidade.CodigoCliente,
+                    Total = entidade.Total,
+                    Produtos = JsonConvert.DeserializeObject<ICollection<VendaProdutos>>(entidade.JsonProdutos)
+                };
+
+                if (entidade.Codigo == null)
+                {
+                    mContext.Venda.Add(objVenda);
+                }
+                else
+                {
+                    mContext.Entry(objVenda).State = EntityState.Modified;
+                }
+
+                mContext.SaveChanges();
+            }
+            else
+            {
+                entidade.ListaClientes  = ListaCliente();
+                entidade.ListaProdutos  = ListaProduto();
+                return View(entidade);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int id)
+        {
+            var ent = new Venda() { Codigo = id };
+            mContext.Attach(ent);
+            mContext.Remove(ent);
+            mContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("LerValorProduto/{CodigoProduto}")]
+        public decimal LerValorProduto(int CodigoProduto)
+        {
+            return mContext.Produto.Where(x => x.Codigo == CodigoProduto).Select(x => x.Valor).FirstOrDefault();
+        }
+    }
+}
---
+++ SistemaVenda/Models/VendaViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVenda.Models
+{
+    public class VendaViewModel
+    {
+        public int? Codigo { get; set; }
+
+        [Required(ErrorMessage="Informe a Data da Venda!")]
+        public DateTime? Data { get; set; }
+
+        [Required(ErrorMessage = "Informe o Cliente!")]
+        public int? CodigoCliente { get; set; }
+        public IEnumerable<SelectListItem> ListaClientes { get; set; }
+
+        public IEnumerable<SelectListItem> ListaProdutos { get; set; }
+
+        public string JsonProdutos { get; set; }
+
+        public decimal Total { get; set; }
+    }
+}
